70a0cc4b58ea54d1fd34cbf7114f8d2fb0c2ef85
Bùi Quang Minh <133204697+buiquangminh2101024@users.noreply.github.com>
2024-05-06 00:43:23 +0700

    Update find.js

--- js/find.js
+++ js/find.js
@@ -5,12 +5,14 @@ function createDeXuat(data, found){
 
     var span = $("<span></span>").css({'color': 'black'}).text(found);
 
-    var a = $("<a></a>").attr({'href' : 'chiTietSanPham.html', 'role': 'button', 'style': 'text-decoration: none; color: rgb(169, 168, 168);'});
+    var a = $("<a></a>").attr({'href' : 'chiTietSanPham.html', 'role': 'button', 'style': 'text-decoration: none; color: rgb(169, 168, 168); font-size: 15px', 'data-bs-toggle': 'tooltip', 'title': data.name});
     a.append("- "); a.append(name.slice(0, f)); a.append(span); a.append(name.slice(f));
     a.click(function(){
         localStorage.setItem('product', JSON.stringify(data));
     })
-    return a;
+    
+    var div = $("<div></div>").attr({'class': 'row w-100 border m-0 overflow-y-hidden', 'style': 'height: 30px'}).append(a);
+    return div;
 }
 $(document).ready(function () {
     $("#find").on('input', function(){
@@ -24,7 +26,6 @@ $(document).ready(function () {
         for(var p1 of products){
             if(re.test(p1.name)){
                 $("#recommend").append(createDeXuat(p1, p1.name.charAt(p1.name.search(re))))
-                $("#recommend").append($("<br>"))
             }
         }
     })
